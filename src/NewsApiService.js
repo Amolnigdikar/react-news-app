3ec6cedbae55ce561c3111809c3a903d503d264c
Amol Nigdikar <35647064+Amolnigdikar@users.noreply.github.com>
2020-09-04 14:46:00 +0530

    Update NewsApiService.js

--- src/NewsApiService.js
+++ src/NewsApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const url =
-  "https://newsapi.org/v2/top-headlines?country=in&apiKey=9b1c4a33ee024e438fbd870467634c81";
+  "https://newsapi.org/v2/top-headlines?country=in&apiKey=add_your_api_key_here";
 
 export const fetchNews = async () => {
   const data = await axios.get(url);
@@ -11,7 +11,7 @@ export const fetchNews = async () => {
 export const searchNews = async (search) => {
   console.log(search);
   const data = await axios.get(
-    `https://newsapi.org/v2/top-headlines?q=${search}&apiKey=9b1c4a33ee024e438fbd870467634c81`
+    `https://newsapi.org/v2/top-headlines?q=${search}&apiKey=add_your_api_key_here"`
   );
   return data;
 };
